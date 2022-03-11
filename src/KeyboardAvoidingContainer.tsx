4e4469055ac291d691b0943196d31cd4a3fe477c
Tareq Aziz <tareqaziz0065@gmail.com>
2022-03-12 02:17:52 +0600

    Update KeyboardAvoidingContainer.tsx

--- src/KeyboardAvoidingContainer.tsx
+++ src/KeyboardAvoidingContainer.tsx
@@ -218,7 +218,7 @@ export function useKeyboardAvoidingContainerProps<
         if (keyboardLayoutRef.current) return
 
         const {endCoordinates: newKeyboardLayout} = event
-        const newFocusedTextInputNodeHandle = NativeTextInput.State.currentlyFocusedField()
+        const newFocusedTextInputNodeHandle = NativeTextInput.State.currentlyFocusedField() ? findNodeHandle(TextInput.State.currentlyFocusedInput()) : TextInput.State.currentlyFocusedField();
         const newStickyFooterNodeHandle = findNodeHandle(
           stickyFooterRef.current,
         )
